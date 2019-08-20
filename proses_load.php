f5a67cf3110a25c22665aabe0218a62797bce091
ekayuliasr <ekayuliaasari@gmail.com>
2019-08-20 16:32:50 +0700

    ajax

---
+++ proses_load.php
@@ -0,0 +1,4 @@
+<? if ($_GET['id'] == '1') : ?>
+	AJAX adalah singkatan dari Asynchronous JavaScript And XML. AJAX bukan bahasa pemrograman. Aplikasi AJAX mungkin menggunakan XML untuk mengangkut data, namun umumnya untuk mengangkut data sebagai teks biasa atau teks JSON.
+
+<? endif; ?>
